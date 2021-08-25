55b706a3175b3e33c0c3bc846e68143770df66a3
lukey-d <68032934+lukey-d@users.noreply.github.com>
2021-08-25 13:34:45 +1200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='BVT Engineering Methods',
+    version='0.1dev',
+    packages=['NZS_1170_5',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+   
+)
