91ae0f3e4bc00d8811cb8a3fac4abf3ed72593af
lukey-d <68032934+lukey-d@users.noreply.github.com>
2021-08-25 13:37:36 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='BVT Engineering Methods',
     version='0.1dev',
-    packages=['NZS_1170_5',],
+    py_modules=['NZS_1170_5'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
    
 )
