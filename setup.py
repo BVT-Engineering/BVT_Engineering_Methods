93e66dabb035a7b0294b0f7b33c3618cb600862a
lukey-d <68032934+lukey-d@users.noreply.github.com>
2021-08-26 08:28:16 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='example',
-    version='0.1.0',
-    packages=find_packages(include=['exampleproject', 'exampleproject.*'])
+    version='0.1dev',
+    packages=find_packages()
 )
