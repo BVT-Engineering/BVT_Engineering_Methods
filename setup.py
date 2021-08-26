1e5c71a8545ad29eeb04af6cd453a1eb7f80e6e0
Luke Doyle <luke.doyle@bvt.co.nz>
2021-08-27 09:22:56 +1200

    Test

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="BVT_Engineering_Methods",
     version="0.4dev",
-    py_modules=["NZS_1170_5", "test"],
+    py_modules=["NZS_1170_5", "test_module"],
 )
