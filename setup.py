88ea7e6203da5bca673156436a0c298c5f19a727
Luke Doyle <luke.doyle@bvt.co.nz>
2021-08-26 14:52:51 +1200

    Edit setup.py file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="BVT_Engineering_Methods",
-    version="0.1dev",
+    version="0.3dev",
     package_dir={"": "BVT_Engineering_Methods"},
     packages=setuptools.find_packages(where="BVT_Engineering_Methods"),
 )
