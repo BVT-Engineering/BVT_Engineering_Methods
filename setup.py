7304b9e65bfa23a27bc86537a23e2d0e4c5573f4
Luke Doyle <luke.doyle@bvt.co.nz>
2021-08-26 14:50:04 +1200

    Edit setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='BVT_Engineering_Methods',
-    version='0.1dev',
-    py_modules = ['NZS_1170_5']
+    name="BVT_Engineering_Methods",
+    version="0.1dev",
+    package_dir={"": "BVT_Engineering_Methods"},
+    packages=setuptools.find_packages(where="BVT_Engineering_Methods"),
 )
