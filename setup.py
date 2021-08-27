8d895727aaa5321d750967c513adc00c5dfd1e51
mattbishop-bvtengineering <89442276+mattbishop-bvtengineering@users.noreply.github.com>
2021-08-28 07:28:55 +1200

    Update setup.py
    Add asnzs_1170_0_2002.py into setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="BVT_Engineering_Methods",
     version="0.5dev",
-    py_modules=["NZS_1170_5"],
+    py_modules=["NZS_1170_5","asnzs_1170_0_2002"],
 )
