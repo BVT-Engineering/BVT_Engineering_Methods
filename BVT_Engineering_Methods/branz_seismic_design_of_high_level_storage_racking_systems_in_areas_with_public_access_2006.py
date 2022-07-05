8561a48864e25cde7d8da6a06fdbcf18596a65a6
sambamford-bvtengineering <104741136+sambamford-bvtengineering@users.noreply.github.com>
2022-07-06 09:26:50 +1200

    Update branz_seismic_design_of_high_level_storage_racking_systems_in_areas_with_public_access_2006.py
    BVT_Engineering_Methods install commented out

--- BVT_Engineering_Methods/branz_seismic_design_of_high_level_storage_racking_systems_in_areas_with_public_access_2006.py
+++ BVT_Engineering_Methods/branz_seismic_design_of_high_level_storage_racking_systems_in_areas_with_public_access_2006.py
@@ -23,7 +23,7 @@ Reviewed -
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%capture
-!pip install git+https://github.com/sambamford-bvtengineering/BVT_Engineering_Methods.git
+# pip install git+https://github.com/sambamford-bvtengineering/BVT_Engineering_Methods.git
 #BVT Standards function library, https://github.com/BVT-Engineering/BVT_Engineering_Methods 
 from BVT_Engineering_Methods import asnzs_1170_0_2002 as ASNZS_1170_0
 # #import AS/NZS 1170.0 Structural design actions - Part 0: General principles, https://colab.research.google.com/drive/1vXdyWK3uu6MncNY87YmABolL_-iVVzg2
