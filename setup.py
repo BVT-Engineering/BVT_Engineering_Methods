82671a4933eba013dd033353dd59ac407db05a30
mattbishop-bvtengineering <89442276+mattbishop-bvtengineering@users.noreply.github.com>
2022-11-29 09:42:22 +1300

    Update setup.py
    Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BVT_Engineering_Methods",
-    version="0.7dev",
+    version="0.8dev",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
