1a36d34024924caaf13461876904095147ca2f5f
Luke Doyle <68032934+lukedoyle7@users.noreply.github.com>
2022-11-29 10:45:01 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BVT_Engineering_Methods",
-    version="0.8dev",
+    version="0.9dev",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
